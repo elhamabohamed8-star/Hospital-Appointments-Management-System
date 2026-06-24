64c135626a37b680de68f04010488dc19bf7b0bf
elhamabohamed8-star <elhamabohamed8@gmail.com>
2026-06-24 18:07:06 +0300

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -50,15 +50,8 @@ public class FileManager {
             String line;
            while ((line = reader.readLine())!= null) {
                 if (!line.trim().isEmpty()) data.add(line.split(","));
-          /* while ((line = reader.readLine()) != null) {
-    if (!line.trim().isEmpty()) {
-        line = line.replace("٠","0").replace("١","1").replace("٢","2")
-                   .replace("٣","3").replace("٤","4").replace("٥","5")
-                   .replace("٦","6").replace("٧","7").replace("٨","8")
-                   .replace("٩","9");
-        data.add(line.split(","));
-    }
-}*/
+          
+   
            }
         }
  return data;
